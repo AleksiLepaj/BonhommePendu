46b8cc1adec2a3f13b6db9b702e2da82f75f7f38
Alexis <2175497@cegepmontpetit.ca>
2024-11-01 08:53:18 -0400

    FCT Game Events
    Configuration de tous les GameEvents

--- BonhommePendu/Events/RevealLetterEvent.cs
+++ BonhommePendu/Events/RevealLetterEvent.cs
@@ -5,10 +5,22 @@ namespace BonhommePendu.Events
     // Un événement à créer pour CHAQUE positon d'une lettre que l'on veut révéler (alors il faut faire plusieurs événements si la lettre est là plusieurs fois!)
     public class RevealLetterEvent : GameEvent
     {
+        public char Letter { get; set; }
+        public int Index { get; set; }
+
         public RevealLetterEvent(GameData gameData, char letter, int index)
         {
             // Conseil: Vous pouvez utiliser gameData.RevealLetter mettre à jour gameData
             // Conseil: Vous pouvez utiliser gameData.HasGuessedTheWord pour savoir si c'est une victoire
+            Index = index;
+            Letter = gameData.RevealLetter(index);
+            if (gameData.HasGuessedTheWord)
+            {
+                Events = new List<GameEvent>
+                {
+                    new WinEvent(gameData)
+                };
+            }
         }
     }
 }
